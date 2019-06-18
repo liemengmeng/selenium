4e62880c9744d1fda1a80448cb08798ecaa159a4
John Doe <john@sample.com>
2019-06-18 17:34:36 +0800

    提交

--- src/test/java/logic/testcase.java
+++ src/test/java/logic/testcase.java
@@ -1,18 +1,29 @@
 package logic;
 
 
-import MyAssert.MyAssert;
+
+import com.fengzhaung.ScreenShot;
 import log4j.LoggerControler;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.Test;
 
 
-import java.text.DateFormat;
 
 public class testcase {
 LoggerControler log= LoggerControler.getLogger(testcase.class);
 
     @Test
     public static void test(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\Application\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.baidu.com/");
+        driver.findElement( By.xpath("abc")).click();
+        ScreenShot.screenShots();
+
 
 
 
