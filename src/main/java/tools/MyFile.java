a277aeb9c79b42d6957837be7f7f184b2a8a1ff7
John Doe <john@sample.com>
2019-06-18 10:46:25 +0800

    提交

--- src/main/java/tools/MyFile.java
+++ src/main/java/tools/MyFile.java
@@ -3,9 +3,11 @@ package tools;
 import java.io.File;
 
 public class MyFile {
+    //判断文件是否存在
     public static boolean fileExists(String filePath){
         return  new File(filePath).exists();
     }
+    //文件不存在创建一个新的文件
     public static void creatFile(String filePath){
         MyFile myFile=new MyFile();
         if (!myFile.fileExists(filePath)){
--- src/test/java/logic/testcase.java
+++ src/test/java/logic/testcase.java
@@ -3,16 +3,21 @@ package logic;
 
 import log4j.LoggerControler;
 import org.testng.annotations.Test;
-import tools.RandomNum;
+
+
+import java.text.DateFormat;
 
 public class testcase {
 LoggerControler log= LoggerControler.getLogger(testcase.class);
 
     @Test
-    public void show(){
-        RandomNum.getNumRandom(5);
-log.error("这是一个error");
+    public static void test(){
+
 
     }
 
-}
+    }
+
+
+
+
